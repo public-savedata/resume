ed88e943b4e3a6e3a684fbe100bd4c522c67930c
public-savedata <110449998+public-savedata@users.noreply.github.com>
2022-08-02 11:22:31 +0430

    first update

---
+++ index.html.element.js
@@ -0,0 +1,6 @@
+// !! Warning : Auto Generated Content 
+export class HtmlIndex_Element {
+    relative = "/index.html";
+    Report = document.querySelector("pre#report");
+}
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguaHRtbC5lbGVtZW50LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiaW5kZXguaHRtbC5lbGVtZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLHVDQUF1QztBQUV2QyxNQUFNLE9BQU8saUJBQWlCO0lBRTFCLFFBQVEsR0FBRyxhQUFhLENBQUE7SUFDcEIsTUFBTSxHQUFHLFFBQVEsQ0FBQyxhQUFhLENBQUMsWUFBWSxDQUFnQixDQUFDO0NBQ3BFIn0=
---
+++ index.html.script.js
@@ -0,0 +1,3 @@
+import { ImageSlider } from "./effects/image-slider/image-slider.js";
+new ImageSlider();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguaHRtbC5zY3JpcHQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC5odG1sLnNjcmlwdC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxPQUFPLEVBQUUsV0FBVyxFQUFFLE1BQU0sd0NBQXdDLENBQUE7QUFFcEUsSUFBSSxXQUFXLEVBQUUsQ0FBQyJ9
